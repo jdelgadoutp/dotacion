b5c6b1d76d30b5760f904bca1fbad108d1234a7b
jdelgadoutp <j.delgado@utp.edu.co>
2020-06-18 23:00:15 -0500

    se organiza paquetes

--- src/main/java/co/edu/utp/dotaciones/model/repository/ProductosRepository.java
+++ src/main/java/co/edu/utp/dotaciones/model/repository/ProductosRepository.java
@@ -1,5 +1,9 @@
 package co.edu.utp.dotaciones.model.repository;
 
-public interface ProductosRepository {
+import org.springframework.data.repository.CrudRepository;
+
+import co.edu.utp.dotaciones.model.entity.Productos;
+
+public interface ProductosRepository extends CrudRepository<Productos, String>{
 
 }
